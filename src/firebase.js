af359c638b0b0a9e07d509bf15739ab4dbe2298f
Wandile <wandile.mlambo@yahoo.com>
2023-03-01 16:44:33 +0200

    responsive

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth"
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpLCVh-VENIHpScEDFOZj17DdC3tHk_Z0",
-  authDomain: "clone-w.firebaseapp.com",
-  projectId: "clone-w",
-  storageBucket: "clone-w.appspot.com",
-  messagingSenderId: "842544872228",
-  appId: "1:842544872228:web:c1140f5caf87fcfa46d1b1"
+  apiKey: "AIzaSyAk9JgxZ7_chZ1QgLKcq75cUocgqHuL1gs",
+  authDomain: "clone-8f3ad.firebaseapp.com",
+  projectId: "clone-8f3ad",
+  storageBucket: "clone-8f3ad.appspot.com",
+  messagingSenderId: "658398561256",
+  appId: "1:658398561256:web:3280db1c03b0df59d85a5d"
 };
 
 
